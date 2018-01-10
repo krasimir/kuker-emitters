4b86a6972e039c3ee8289abb8e8046edd0913203
Krasimir Tsonev <krasimir@outset.ws>
2018-01-10 08:02:36 +0200

    VueEmitter

---
+++ src/VueEmitter.js
@@ -0,0 +1,76 @@
+import sanitize from './helpers/sanitize';
+import createMessenger from './helpers/createMessenger';
+
+const HOOK_KEY = '__KUKER_VUE_HOOK__';
+var detectAttempts = 100;
+
+const detect = function (callback) {
+  detectAttempts -= 1;
+
+  if (window[HOOK_KEY] && window[HOOK_KEY].listen) {
+    callback(null, window[HOOK_KEY]);
+  } else {
+    if (detectAttempts === 0) {
+      callback('VueEmitter: Kuker extension not installed or it is disabled.');
+    }
+    setTimeout(() => detect(callback), 10);
+  }
+};
+
+export default function VueEmitter() {
+  if (typeof window === 'undefined') return;
+
+  const message = createMessenger('VueEmitter');
+  const send = data => message(sanitize(data));
+
+  detect((error, hook) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    hook.listen((type, payload) => {
+      // console.log(type);
+      if (type === 'flush') {
+        send({
+          type: '@@vue_flush',
+          state: payload,
+          emitter: 'Vue'
+        });
+      } else if (type === 'ready') {
+        send({
+          type: '@@vue_ready',
+          version: payload,
+          state: {},
+          emitter: 'Vue'
+        });
+      } else if (type === 'vuex:mutation') {
+        try {
+          send({
+            type: '@@vuex_vuex:mutation',
+            state: JSON.parse(payload.snapshot),
+            mutation: payload.mutation,
+            emitter: 'Vuex'
+          });
+        } catch (error) {
+          console.error('VueEmitter: can not JSON.parse the payload of `vuex:mutation` event.');
+        }
+      } else if (type === 'vuex:init') {
+        try {
+          send({
+            type: '@@vuex_vuex:init',
+            state: JSON.parse(payload),
+            emitter: 'Vuex'
+          });
+        } catch (error) {
+          console.error('VueEmitter: can not JSON.parse the payload of `vuex:init` event.');
+        }
+      } else {
+        send({
+          type: '@@vue_' + type,
+          state: payload,
+          emitter: 'Vue'
+        });
+      }
+    });
+  });
+};
